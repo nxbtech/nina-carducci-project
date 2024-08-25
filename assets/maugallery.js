c3916f72f32dc579286a8755d90e9605a4a2bd8a
nxbtech <159707021+nxbtech@users.noreply.github.com>
2024-08-25 09:30:50 +0200

    Signed-off-by: nxbtech <dernieres modifications apportées au projet Nina-Carducci>

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -23,6 +23,21 @@ function createLightBox(gallery,lightboxId,navigation){gallery.append(`<div clas
 function attachListeners(gallery,options){gallery.on("click",".gallery-item",function(){if(options.lightBox&&$(this).prop("tagName")==="IMG"){openLightBox($(this),options.lightboxId)}});gallery.on("click",".nav-link",filterByTag);gallery.on("click",".mg-prev",()=>navigateImage(options.lightboxId,'prev'));gallery.on("click",".mg-next",()=>navigateImage(options.lightboxId,'next'))}
 function openLightBox(element,lightboxId){$(`#${lightboxId}`).find(".lightboxImage").attr("src",element.attr("src"));$(`#${lightboxId}`).modal("toggle")}
 function showTags(gallery,position,tags){let tagItems='<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all">Tous</span></li>';$.each(tags,function(index,value){tagItems+=`<li class="nav-item"><span class="nav-link" data-images-toggle="${value}">${value}</span></li>`});const tagsRow=`<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;if(position==="bottom"){gallery.append(tagsRow)}else if(position==="top"){gallery.prepend(tagsRow)}else{console.error(`Unknown tags position: ${position}`)}}
-function filterByTag(){if($(this).hasClass("active-tag")){return}
-$(".active-tag").removeClass("active active-tag");$(this).addClass("active-tag");const tag=$(this).data("images-toggle");$(".gallery-item").each(function(){const $itemColumn=$(this).parents(".item-column");if(tag==="all"||$(this).data("gallery-tag")===tag){$itemColumn.show(300)}else{$itemColumn.hide()}})}
+function filterByTag(){
+    if($(this).hasClass("active-tag")){
+        return;
+    }
+    $(".active-tag").removeClass("active active-tag");
+    $(this).addClass("active active-tag");
+    
+    const tag = $(this).data("images-toggle");
+    $(".gallery-item").each(function(){
+        const $itemColumn = $(this).parents(".item-column");
+        if(tag === "all" || $(this).data("gallery-tag") === tag){
+            $itemColumn.show(300);
+        } else {
+            $itemColumn.hide();
+        }
+    });
+}
 function navigateImage(lightboxId,direction){const $lightboxImage=$(`#${lightboxId}`).find(".lightboxImage");const currentSrc=$lightboxImage.attr("src");const activeTag=$(".tags-bar span.active-tag").data("images-toggle");let imagesCollection=[];$(".item-column").each(function(){const $img=$(this).children("img");if(activeTag==="all"||$img.data("gallery-tag")===activeTag){imagesCollection.push($img)}});const currentIndex=imagesCollection.findIndex(img=>img.attr("src")===currentSrc);let nextIndex=(direction==='prev')?currentIndex-1:currentIndex+1;if(nextIndex<0)nextIndex=imagesCollection.length-1;if(nextIndex>=imagesCollection.length)nextIndex=0;$lightboxImage.attr("src",imagesCollection[nextIndex].attr("src"))}};$.fn.mauGallery.defaults={columns:3,lightBox:!0,lightboxId:null,showTags:!0,tagsPosition:"bottom",navigation:!0}})(jQuery)
